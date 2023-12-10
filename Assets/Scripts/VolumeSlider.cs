f60e192ff34ecc99e9553624c8b3757aad973e90
Anthony.B <anthony.boll@artfx.fr>
2023-12-10 16:47:30 +0100

    finish volume slider

--- Assets/Scripts/VolumeSlider.cs
+++ Assets/Scripts/VolumeSlider.cs
@@ -5,18 +5,36 @@ using UnityEngine.UI;
 
 public class VolumeSlider : MonoBehaviour
 {
-    public static VolumeSlider instance;
     [SerializeField] Slider volumeSlider;
     public float volume;
 
     private void Start()
     {
-        instance = this;
+        if(!PlayerPrefs.HasKey("volume"))
+        {
+            PlayerPrefs.SetFloat("volume", 1);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
     }
 
     public void ChangeVolume()
     {
         volume = volumeSlider.value;
         AudioListener.volume = volume;
+        Save();
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("volume", volume);
+    }
+
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("volume");
     }
 }
